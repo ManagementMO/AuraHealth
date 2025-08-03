5abd11fd3410004a8f5ccb40a7b65f299dd41062
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-08-02 20:55:00 -0400

    frontend interface call working

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FYk2w2/8pTR+t8alHITUfM9HTZiYMEeyGuz32FGN2w0=\"\n}"
--- .next/static/chunks/src_app_check-in_page_tsx_aadfe7bb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_215b0bf2._.js",
-  "static/chunks/src_2b0a0c48._.js"
-],
-    source: "dynamic"
-});
